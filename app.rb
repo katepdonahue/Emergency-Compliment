b2fcb56639038df0bb7dff627affd816c3a35e4d
Kate <katepdonahue@gmail.com>
2013-12-09 17:57:02 -0500

    Add background image and css

--- app.rb
+++ app.rb
@@ -4,9 +4,10 @@ require './lib/compliment'
 
 
 class App < Sinatra::Application
+  attr_reader :picture
 
   get '/compliments' do
-    new_compliment = Compliment.new.
+    new_compliment = Compliment.new
     @compliment = new_compliment.message
     @picture = new_compliment.image
     erb :index
